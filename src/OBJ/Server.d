16a071a36adedd2a9405c417070f53e3900362d8
Ocyn <niko.ocyn@gmail.com>
2024-08-15 13:10:57 +0200

    Main func finally clean, splitted all server part into dedicated functions, added infos comments for better global code understanding

---
+++ src/OBJ/Server.d
@@ -0,0 +1,1 @@
+src/OBJ/Server.o: src/Server.cpp src/Server.hpp src/Exceptions.hpp
--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,49 @@
 /*   By: ocyn <ocyn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 11:43:14 by ocyn              #+#    #+#             */
-/*   Updated: 2024/08/15 12:34:18 by ocyn             ###   ########.fr       */
+/*   Updated: 2024/08/15 13:08:51 by ocyn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
+void	_receivingServ(Server &server);
+void	_addFdClient(Server &server, int &max_sd);
+int		_watchFds(Server &server, int &max_sd);
+int		_newConnections(Server &server);
+
+int main(int ac, char **av)
+{
+	if (ac < 2)
+		return (1);
+	Server server(av[1], "password"); // Init server
+	server.startServer(av[1]); // Start the server
+
+	fd_set &fdset = server.getFdSet();
+	int max_sd = server.getSocket();
+	while (true)
+	{
+		FD_ZERO(&fdset);
+		FD_SET(server.getSocket(), &fdset);
+
+		// Adding new Clients to list
+		_addFdClient(server, max_sd);
+		// Watching file descriptors
+		if (_watchFds(server, max_sd))
+			break;
+		// New coming connections handling
+		if (_newConnections(server))
+			break ;
+		// Receiving clients datas
+		_receivingServ(server);
+	}
+	return 0;
+}
+
 /*
 Loop for each clients to check received messages
 */
-void	_receivingServ(Server &server, fd_set &fdset)
+void	_receivingServ(Server &server)
 {
 		std::vector<int> clients = server.getClientsList();
 		for (std::vector<int>::iterator it = clients.begin(); it != clients.end(); )
@@ -23,7 +56,7 @@ void	_receivingServ(Server &server, fd_set &fdset)
 			int client_fd = *it;
 
 			// Check new incoming datas
-			if (FD_ISSET(client_fd, &fdset))
+			if (FD_ISSET(client_fd, &server.getFdSet()))
 			{
 				char buffer[1024];
 				int valread = recv(client_fd, buffer, 1024, 0);
@@ -39,11 +72,11 @@ void	_receivingServ(Server &server, fd_set &fdset)
 					close(client_fd);
 					it = clients.erase(it);
 				}
-				else 
+				else
 				{
 					// Affiche le message reçu du client
 					std::cout << "Message reçu du client, socket fd: " << client_fd << std::endl;
-					std::cout << "Contenu: " << buffer << std::endl;
+					std::cout << "Contenu: [ " << buffer << " ]" << std::endl;
 					std::cout << "Size: " << valread << std::endl;
 					++it;
 				}
@@ -53,51 +86,43 @@ void	_receivingServ(Server &server, fd_set &fdset)
 		}
 }
 
-int main(int ac, char **av)
+void	_addFdClient(Server &server, int &max_sd)
 {
-	if (ac < 2)
-		return (1);
-	Server server(av[1], "password"); // Init server
-	server.startServer(av[1]); // Start the server
-
-	fd_set &fdset = server.getFdSet();
-	int max_sd = server.getSocket();
-	while (true)
+	vector<int> clients = server.getClientsList();
+	for (vector<int>::iterator it = clients.begin(); it != clients.end(); ++it)
 	{
-		FD_ZERO(&fdset);
-		FD_SET(server.getSocket(), &fdset);
-		vector<int> clients = server.getClientsList();
+		int client_fd = *it;
+		FD_SET(client_fd, &server.getFdSet());
+		if (client_fd > max_sd)
+			max_sd = client_fd;
+	}
+}
 
-		// Adding clients file descriptors
-		for (vector<int>::iterator it = clients.begin(); it != clients.end(); ++it)
-		{
-			int client_fd = *it;
-			FD_SET(client_fd, &fdset);
-			if (client_fd > max_sd)
-				max_sd = client_fd;
-		}
+int	_watchFds(Server &server, int &max_sd)
+{
+	int activity = select(max_sd + 1, &server.getFdSet(), NULL, NULL, NULL);
 
-		// Watching file descriptors
-		int activity = select(max_sd + 1, &fdset, NULL, NULL, NULL);
-		if (activity < 0 && errno != EINTR) {
-			std::cerr << "Erreur lors de l'utilisation de select()" << std::endl;
-			break;
-		}
+	if (activity < 0 && errno != EINTR) {
+		std::cerr << "Erreur lors de l'utilisation de select()" << std::endl;
+		return (1);
+	}
+	return (0);
+}
 
-		// New coming connections handling
-		if (FD_ISSET(server.getSocket(), &fdset)) {
+int		_newConnections(Server &server)
+{
+	if (FD_ISSET(server.getSocket(), &server.getFdSet()))
+	{
 			struct sockaddr_in client_addr;
 			socklen_t client_len = sizeof(client_addr);
 			int newsockfd = accept(server.getSocket(), (struct sockaddr*)&client_addr, &client_len);
 			if (newsockfd < 0) {
 				std::cerr << "Erreur lors de l'acceptation de la connexion" << std::endl;
-				break;
+				return (1);
 			}
 			std::cout << "Nouvelle connexion acceptée, socket fd: " << newsockfd << std::endl;
 			server.getClientsList().push_back(newsockfd);
 
 		}
-		_receivingServ(server, fdset);
-	}
-	return 0;
+	return (0);
 }
