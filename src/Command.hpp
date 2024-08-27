128d53b2834255b3d964640d711d79554afaaa9a
nonomex <81557850+nonomex@users.noreply.github.com>
2024-08-27 17:15:55 +0200

    Update Command.hpp
    add pass

--- src/Command.hpp
+++ src/Command.hpp
@@ -5,3 +5,4 @@
 void nick(Server& server, Client& client, const char *buffer);
 void user(Server& server, Client& client, const char *buffer);
 void privmsg(Server& server, Client& client, const char *buffer);
+void pass(Server& server, Client& client, const char *buffer);
