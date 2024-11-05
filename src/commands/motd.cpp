f4234a38e715048766fde9f9c62d1e999f64a849
Nephtys <lucie.camerlynck@hotmail.com>
2024-11-05 09:00:50 +0100

    Edited motd.txt into configuration file to be more subject friendly

--- src/commands/motd.cpp
+++ src/commands/motd.cpp
@@ -6,7 +6,7 @@
 /*   By: lcamerly <lcamerly@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 18:35:55 by jcuzin            #+#    #+#             */
-/*   Updated: 2024/11/05 08:37:54 by lcamerly         ###   ########.fr       */
+/*   Updated: 2024/11/05 08:54:53 by lcamerly         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	motd(Server& server, Client& client)
 {
-	std::ifstream	file("motd.txt");
+	std::ifstream	file("motd.conf");
 	if (!file.is_open())
 		return (server.sendData(client.getClientFd(), getNumericReply(client, 422, "")));
 	string	line;
